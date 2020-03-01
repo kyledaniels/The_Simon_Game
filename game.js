f1a1c52f9512b8fb1ccf73ea5ba1f5831da1ad79
kyle67 <daniels.kyle@gmail.com>
2020-03-01 13:18:28 -0500

    Added animation to buttons

--- game.js
+++ game.js
@@ -22,11 +22,11 @@ $(".btn").click(function() {
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
-
   var randomChosenColor = buttonColours[randomNumber];
-
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
 
 }
 
-$("button").randomChosenColor

e080a659c32d38cd86959f90e2b40ffef429c435
kyle67 <daniels.kyle@gmail.com>
2020-03-01 13:52:50 -0500

    Added event listener to buttons

--- game.js
+++ game.js
@@ -9,6 +9,8 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
+//Add event listener to document for when
+// buttons are pushed by user
 $(document).keypress(function() {
     if (!started) {
       $("#level-title").text("Level " + level);

731e34cc580efc04d58231356012e62e45234c43
kyle67 <daniels.kyle@gmail.com>
2020-03-01 13:00:48 -0500

    Created array to hold use pattern of clicks

--- game.js
+++ game.js
@@ -1,16 +1,22 @@
 // Create an array to hold button colors
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-//    Create an array to hold color patterns
+//Create an array to hold color patterns
+// Create an array to hold usersClicked patterns
 var gamePattern = [];
+var userClickedPattern = [];
 
-//create a new function called nextSequence
+
+
+//Create a new function called nextSequence
 function nextSequence() {
 
   var randomNumber = Math.floor(Math.random() * 4);
 
-  var randomChosenColour = buttonColours[randomNumber];
+  var randomChosenColor = buttonColours[randomNumber];
 
   gamePattern.push(randomChosenColour);
 
 }
+
+$("button").randomChosenColor

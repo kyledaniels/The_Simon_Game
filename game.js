b7ad67eb6ea51379b7b46a3a9ebcce380bd0d7f8
kyle67 <daniels.kyle@gmail.com>
2020-03-01 13:19:25 -0500

    Added sound to game

--- game.js
+++ game.js
@@ -27,6 +27,7 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
 

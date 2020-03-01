bbd94f0b929c24264f4e1f445ce1651ab4fa2446
kyle67 <daniels.kyle@gmail.com>
2020-03-01 15:27:26 -0500

    Created function to check user response

--- game.js
+++ game.js
@@ -34,6 +34,29 @@ $(".btn").click(function() {
     checkAnswer(userClickedPattern.length-1);
   });
 
+//Created function to check user answer
+
+  function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
+
 //Create a new function called nextSequence
 function nextSequence() {
 

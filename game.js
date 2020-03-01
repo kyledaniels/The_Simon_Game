23de110b4ac6b4498b37e74266aa9d406ae4a499
kyle67 <daniels.kyle@gmail.com>
2020-03-01 13:09:00 -0500

    created callback function to determine buttons clicked

--- game.js
+++ game.js
@@ -6,7 +6,17 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+// Select button and create a callback function
+// to determine which button is being clicked.
+$(".btn").click(function() {
 
+    var userChosenColour = $(this).attr("id");
+  
+    userClickedPattern.push(userChosenColour);
+  
+    //console.log(userClickedPattern);
+  
+  });
 
 //Create a new function called nextSequence
 function nextSequence() {
